fa26f14c3219a67887de745732fff8eb7104d637
Mrmore <matong38@gmail.com>
2013-05-03 13:57:40 +0800

    添加一些工具类```

---
+++ ControlLibrary/Tools/Encryption/Sha256.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+
+namespace ControlLibrary.Tools
+{
+    public static class Sha256
+    {
+        public static string EncodeToBase64String(string input)
+        {
+            var algo = HashAlgorithmProvider.OpenAlgorithm("SHA256");
+            var buff = CryptographicBuffer.ConvertStringToBinary(input, BinaryStringEncoding.Utf8);
+            var hashed = algo.HashData(buff);
+            return CryptographicBuffer.EncodeToBase64String(hashed);
+        }
+
+        public static string EncodeToHexString(string input)
+        {
+            var algo = HashAlgorithmProvider.OpenAlgorithm("SHA256");
+            var buff = CryptographicBuffer.ConvertStringToBinary(input, BinaryStringEncoding.Utf8);
+            var hashed = algo.HashData(buff);
+            return CryptographicBuffer.EncodeToHexString(hashed);
+        }
+    }
+}
---
+++ ControlLibrary/Tools/Thread.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlLibrary.Tools
+{
+    public class Thread
+    {
+        public static void Sleep(int ms)
+        {
+            new System.Threading.ManualResetEvent(false).WaitOne(ms);
+        }
+
+        public static void Sleep(TimeSpan ts)
+        {
+            new System.Threading.ManualResetEvent(false).WaitOne(ts);
+        }
+    }
+}
