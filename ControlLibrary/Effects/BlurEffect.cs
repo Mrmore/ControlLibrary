9334f94a2a537ba2d26ab3ae2b20d4082de09d83
Mrmore <matong38@gmail.com>
2013-05-14 20:01:05 +0800

    增加位图的扩展方法，增加一个模糊滤镜，在原有的MoveOutControl控件测试的页面，测试模糊滤镜，现在的版本还有Bug```

---
+++ ControlLibrary/Effects/BlurEffect.cs
@@ -0,0 +1,45 @@
+﻿using ControlLibrary.Tools.WriteableBitmapExtension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace ControlLibrary.Effects
+{
+    public static class BlurEffect
+    {
+        public static WriteableBitmap WriteableBitmapBlur(this WriteableBitmap writeableBitmapOld, int[,] GaussianBlur)
+        {
+            WriteableBitmap writeableBitmapBlur = null;
+            if (GaussianBlur == null)
+            {
+                writeableBitmapBlur = WriteableBitmapExtensions.Convolute(writeableBitmapOld, WriteableBitmapExtensions.KernelGaussianBlur5x5);
+            }
+            else
+            {
+                writeableBitmapBlur = WriteableBitmapExtensions.Convolute(writeableBitmapOld, GaussianBlur);
+            }
+            return writeableBitmapBlur;
+        }
+
+        public static WriteableBitmap WriteableBitmapBlur(this WriteableBitmap writeableBitmapOld, int kernelWidth, int kernelHeight)
+        {
+            WriteableBitmapConvolutionExtensions.GaussianBlur(writeableBitmapOld, kernelWidth, kernelHeight);
+            return writeableBitmapOld;
+        }
+
+        public static WriteableBitmap WriteableBitmapBlur(this WriteableBitmap writeableBitmapOld)
+        {
+            WriteableBitmapConvolutionExtensions.GaussianBlur(writeableBitmapOld);
+            return writeableBitmapOld;
+        }
+
+        public static WriteableBitmap WriteableBitmapBlur(this WriteableBitmap writeableBitmapOld, int range)
+        {
+            WriteableBitmapConvolutionExtensions.BoxBlur(writeableBitmapOld, range);
+            return writeableBitmapOld;
+        }
+    }
+}
--- TestDemoApp/MoveOutControlBlankPage.xaml.cs
+++ TestDemoApp/MoveOutControlBlankPage.xaml.cs
@@ -1,15 +1,18 @@
-﻿using System;
+﻿using ControlLibrary.Effects;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
@@ -21,9 +24,20 @@ namespace TestDemoApp
     /// </summary>
     public sealed partial class MoveOutControlBlankPage : Page
     {
+        private WriteableBitmap wb = null;
         public MoveOutControlBlankPage()
         {
             this.InitializeComponent();
+            this.Loaded += MoveOutControlBlankPage_Loaded;
+        }
+
+        private async void MoveOutControlBlankPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            RandomAccessStreamReference rass = RandomAccessStreamReference.CreateFromUri(new Uri("http://ww1.sinaimg.cn/bmiddle/643be833jw1e4nzv4dc12j20dc0hsq4g.jpg", UriKind.RelativeOrAbsolute));
+            IRandomAccessStreamWithContentType streamRandom = await rass.OpenReadAsync();
+            wb = new WriteableBitmap(1, 1);
+            await wb.SetSourceAsync(streamRandom);
+            this.image.Source = wb;
         }
 
         /// <summary>
@@ -33,11 +47,21 @@ namespace TestDemoApp
         /// 属性通常用于配置页。</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             this.Frame.GoBack();
+        }
+
+        private void bt_Click(object sender, RoutedEventArgs e)
+        {
+            this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    //BlurEffect.WriteableBitmapBlur(wb, null);
+                    BlurEffect.WriteableBitmapBlur(wb, 5);
+                });
         }
     }
 }
