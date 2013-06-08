a342cf42fdf8eea1e1ea0d56fb25d53db1b7d202
Mrmore <matongbmf@163.com>
2013-06-08 12:44:33 +0800

    增加几个异步的扩展方法```

---
+++ ControlLibrary/Extensions/AsyncAwaitExtensions/ScrollViewerAsyncAwaitExtensions.cs
@@ -0,0 +1,156 @@
+﻿using ControlLibrary.Tools.AwaitableUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace ControlLibrary.Extensions
+{
+    /// <summary>
+    /// Extension methods for scrolling a ScrollViewer with a way to await the scoll to complete and with scrolling animation support.
+    /// </summary>
+    public static class ScrollViewerAsyncAwaitExtensions
+    {
+        #region ScrollToVerticalOffsetAsync()
+        /// <summary>
+        /// Scrolls to vertical offset asynchronously.
+        /// </summary>
+        /// <param name="scrollViewer">The scroll viewer.</param>
+        /// <param name="offset">The offset.</param>
+        /// <returns>The task that completes when scrolling is complete.</returns>
+        public static async Task ScrollToVerticalOffsetAsync(this ScrollViewer scrollViewer, double offset)
+        {
+            if (offset < 0)
+                offset = 0;
+
+            if (offset > scrollViewer.ScrollableHeight)
+                offset = scrollViewer.ScrollableHeight;
+
+            var currentOffset = scrollViewer.VerticalOffset;
+            // ReSharper disable CompareOfFloatsByEqualityOperator
+            if (offset == currentOffset)
+                return;
+
+            scrollViewer.ScrollToVerticalOffset(offset);
+
+            if (scrollViewer.VerticalOffset == offset)
+                return;
+
+            if (scrollViewer.VerticalOffset != currentOffset)
+                return;
+            // ReSharper restore CompareOfFloatsByEqualityOperator
+
+            await EventAsync.FromEvent<ScrollViewerViewChangedEventArgs>(
+                eh => scrollViewer.ViewChanged += eh,
+                eh => scrollViewer.ViewChanged -= eh);
+        }
+        #endregion
+
+        #region ScrollToHorizontalOffsetAsync()
+        /// <summary>
+        /// Scrolls to horizontal offset asynchronously.
+        /// </summary>
+        /// <param name="scrollViewer">The scroll viewer.</param>
+        /// <param name="offset">The offset.</param>
+        /// <returns>The task that completes when scrolling is complete.</returns>
+        public static async Task ScrollToHorizontalOffsetAsync(this ScrollViewer scrollViewer, double offset)
+        {
+            if (offset < 0)
+                offset = 0;
+
+            if (offset > scrollViewer.ScrollableWidth)
+                offset = scrollViewer.ScrollableWidth;
+
+            var currentOffset = scrollViewer.HorizontalOffset;
+            // ReSharper disable CompareOfFloatsByEqualityOperator
+            if (offset == currentOffset)
+                return;
+
+            scrollViewer.ScrollToHorizontalOffset(offset);
+
+            if (scrollViewer.HorizontalOffset == offset)
+                return;
+
+            if (scrollViewer.HorizontalOffset != currentOffset)
+                return;
+            // ReSharper restore CompareOfFloatsByEqualityOperator
+
+            await EventAsync.FromEvent<ScrollViewerViewChangedEventArgs>(
+                eh => scrollViewer.ViewChanged += eh,
+                eh => scrollViewer.ViewChanged -= eh);
+        }
+        #endregion
+
+        #region ScrollToVerticalOffsetWithAnimationAsync()
+        /// <summary>
+        /// Scrolls to vertical offset with animation asynchronously.
+        /// </summary>
+        /// <param name="scrollViewer">The scroll viewer.</param>
+        /// <param name="offset">The offset.</param>
+        /// <returns>The task that completes when scrolling is complete.</returns>
+        public static async Task ScrollToVerticalOffsetWithAnimationAsync(this ScrollViewer scrollViewer, double offset)
+        {
+            if (offset < 0)
+                offset = 0;
+
+            if (offset > scrollViewer.ScrollableHeight)
+                offset = scrollViewer.ScrollableHeight;
+
+            var currentOffset = scrollViewer.VerticalOffset;
+            // ReSharper disable CompareOfFloatsByEqualityOperator
+            if (offset == currentOffset)
+                return;
+
+            await scrollViewer.ScrollToVerticalOffsetWithAnimation(offset);
+
+            if (scrollViewer.VerticalOffset == offset)
+                return;
+
+            if (scrollViewer.VerticalOffset != currentOffset)
+                return;
+            // ReSharper restore CompareOfFloatsByEqualityOperator
+
+            await EventAsync.FromEvent<ScrollViewerViewChangedEventArgs>(
+                eh => scrollViewer.ViewChanged += eh,
+                eh => scrollViewer.ViewChanged -= eh);
+        }
+        #endregion
+
+        #region ScrollToHorizontalOffsetWithAnimationAsync()
+        /// <summary>
+        /// Scrolls to horizontal offset with animation asynchronously.
+        /// </summary>
+        /// <param name="scrollViewer">The scroll viewer.</param>
+        /// <param name="offset">The offset.</param>
+        /// <returns>The task that completes when scrolling is complete.</returns>
+        public static async Task ScrollToHorizontalOffsetWithAnimationAsync(this ScrollViewer scrollViewer, double offset)
+        {
+            if (offset < 0)
+                offset = 0;
+
+            if (offset > scrollViewer.ScrollableWidth)
+                offset = scrollViewer.ScrollableWidth;
+
+            var currentOffset = scrollViewer.HorizontalOffset;
+            // ReSharper disable CompareOfFloatsByEqualityOperator
+            if (offset == currentOffset)
+                return;
+
+            await scrollViewer.ScrollToHorizontalOffsetWithAnimation(offset);
+
+            if (scrollViewer.HorizontalOffset == offset)
+                return;
+
+            if (scrollViewer.HorizontalOffset != currentOffset)
+                return;
+            // ReSharper restore CompareOfFloatsByEqualityOperator
+
+            await EventAsync.FromEvent<ScrollViewerViewChangedEventArgs>(
+                eh => scrollViewer.ViewChanged += eh,
+                eh => scrollViewer.ViewChanged -= eh);
+        }
+        #endregion
+    }
+}
---
+++ ControlLibrary/Extensions/AsyncAwaitExtensions/SelectorAsyncAwaitExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace ControlLibrary.Extensions
+{
+    /// <summary>
+    /// Contains an extension method for awaiting selection changes on a Selector control, such as a ListBox, GridView, ListView
+    /// </summary>
+    public static class SelectorAsyncAwaitExtensions
+    {
+        /// <summary>
+        /// Waits for a selection change event in a Selector (eg. a ListBox, a GridView, a ListView).
+        /// </summary>
+        public static async Task<SelectionChangedEventArgs> WaitForSelectionChangedAsync(this Selector selector)
+        {
+            var tcs = new TaskCompletionSource<SelectionChangedEventArgs>();
+
+            // Need to set it to noll so that the compiler does not
+            // complain about use of unassigned local variable.
+            SelectionChangedEventHandler sceh = null;
+
+            sceh = (s, e) =>
+            {
+                selector.SelectionChanged -= sceh;
+                tcs.SetResult(e);
+            };
+
+            selector.SelectionChanged += sceh;
+            return await tcs.Task;
+        }
+    }
+}
