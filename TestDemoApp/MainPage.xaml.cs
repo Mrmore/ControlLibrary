1731ec75f2907cc3282d1d277fd483287a5cb8a9
Mrmore <matongbmf@163.com>
2013-08-07 14:50:47 +0800

    在MainPage 添加 ThreadPoolTimer 实例```

--- TestDemoApp/MainPage.xaml.cs
+++ TestDemoApp/MainPage.xaml.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System.Threading;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -34,6 +36,50 @@ namespace TestDemoApp
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            DelayTimer();
+        }
+
+        /// <summary>
+        /// 延迟定时器
+        /// </summary>
+        private void DelayTimer()
+        {
+            //设置延迟定时器
+            ThreadPoolTimer tptimer = ThreadPoolTimer.CreateTimer(async (timer) =>
+            {
+                await Dispatcher.RunAsync(
+                    CoreDispatcherPriority.High, () =>
+                    {
+                        this.probar1.Value += 20;
+                    });
+            }, TimeSpan.FromMilliseconds(3000));
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            PeriodicTimer();
+        }
+
+        /// <summary>
+        /// 循环定时器
+        /// </summary>
+        private void PeriodicTimer()
+        {
+            //循环定时器
+            ThreadPoolTimer tptimer = ThreadPoolTimer.CreatePeriodicTimer(
+                async (timer) =>
+                {
+                    await Dispatcher.RunAsync(
+                        CoreDispatcherPriority.High, () =>
+                        {
+                            this.probar1.Value = this.probar1.Value + 1;
+                        });
+                },
+                TimeSpan.FromMilliseconds(100));
         }
     }
 }
