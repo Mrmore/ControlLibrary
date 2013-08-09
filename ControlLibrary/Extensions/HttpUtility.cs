bd63373d1b674a09d3742f0e710915080406a857
Mrmore <matongbmf@163.com>
2013-08-09 21:34:44 +0800

    增加Uri的编解码扩展方法```

---
+++ ControlLibrary/Extensions/HttpUtility.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlLibrary.Extensions
+{
+    public static class HttpUtility
+    {
+        /// <summary>
+        /// HTML Encodes a string.
+        /// </summary>
+        /// <param name="buf"></param>
+        /// <returns></returns>
+        /// <remarks></remarks>
+        public static string HtmlEncode(this string buf)
+        {
+            return System.Net.WebUtility.HtmlEncode(buf);
+        }
+
+        /// <summary>
+        /// HTML Decodes a string.
+        /// </summary>
+        /// <param name="buf"></param>
+        /// <returns></returns>
+        /// <remarks></remarks>
+        public static string HtmlDecode(this string buf)
+        {
+            return System.Net.WebUtility.HtmlDecode(buf);
+        }
+
+        /// <summary>
+        /// Encodes text for use in a URL.
+        /// </summary>
+        /// <param name="urlText"></param>
+        /// <returns></returns>
+        /// <remarks></remarks>
+        public static string UrlEncode(this string urlText)
+        {
+            return System.Net.WebUtility.UrlEncode(urlText);
+        }
+
+        /// <summary>
+        /// Decodes text for use in a URL.
+        /// </summary>
+        /// <param name="urlText"></param>
+        /// <returns></returns>
+        /// <remarks></remarks>
+        public static string UrlDecode(this string urlText)
+        {
+            return System.Net.WebUtility.UrlDecode(urlText);
+        }
+    }
+}
